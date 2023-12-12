620b7b63731b4c52927c33ba5f9e8cadbf8f6364
kafilat1210 <cafylhearty@gmail.com>
2023-12-12 09:00:49 -0800

    cmd_change_directory.c

---
+++ cmd_change_directory.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * cd_home - takes in a pointer of char argv
+ * @argv: array of vectors
+ * Return: This function doesn't return
+*/
+
+void cd_home(char **argv)
+{
+	const char *home_dir = getenv("HOME");
+	int i;
+
+	if (home_dir != NULL)
+	{
+		change_directory(home_dir);
+		for(i = 0; argv[i] != NULL; i++)
+			free(argv[i]);
+		free (argv);
+	}
+}
+
+/**
+ * cmd_change_directory - This function takes in pointer to a pointer argv
+ * @argv: Another array of vectors
+ * Return: Return an int value
+ */
+int cmd_change_directory(char **argv)
+{
+	int i;
+
+	if (_str_cmp()argv[0], "cd") ==0)
+	{
+		if (argv[1] == NULL || _str_cmp(argv[1], "~") == 0)
+		{
+			cd_home(argv);
+			return (1);
+		}
+		else if (_str_cmp(argv[1], "-") == 0)
+		{
+			const char *prev_dir = getenv("OLDPWD");
+
+			if (prev_dir != NULL)
+			{
+				change_directory(prev_dir);
+				for (i = 0; argv[i] != NULL; i++)
+					free(argv[i]);
+				free(argv);
+			}
+			return (1);
+		}
+		else
+		{
+			change directory(argv[1]);
+			for (i = 0; argv[i] != NULL; i++)
+				free(argv[1]);
+			free(argv);
+			return (1);
+		}
+	}
+	return (0);
+}
