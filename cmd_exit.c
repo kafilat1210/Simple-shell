93542f1f76092b6937a469341b3792f70ae65229
kafilat1210 <cafylhearty@gmail.com>
2023-12-12 10:43:40 -0800

    cmd_exit.c

---
+++ cmd_exit.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * cmd_exit - Takes in the command and argv arguments
+ * @command: Command passed into the function
+ * @argv: A array of argument vectors
+ */
+void cmd_exit(char *command, char **argv)
+{
+	if (_str_cmp(command, "exit") == 0)
+	{
+		int status = 0;
+
+		if (argv[1] != NULL)
+		{
+			status = atoi(argv[1]);
+		}
+
+		free(command);
+		free(argv);
+		exit(status);
+	}
+}
+
