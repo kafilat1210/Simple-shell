cae66271feb2d7b905a2823b7657973b0946f5a6
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-12-13 22:27:12 -0800

    main.c

---
+++ main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+	string command = NULL, prog_name = NULL;
+	int i;
+
+	(void)argc;
+	prog_name = argv[0];
+
+	while (TRUE)
+	{
+		if (!prompt(&command))
+			continue;
+		argv = set_args(command);
+		if (argv)
+		{
+			if (cmd_set_unset_env(argv))
+				continue;
+			if (cmd_change_directory(argv))
+				continue;
+			execmd(prog_name, argv);
+			if (argv)
+			{
+				for (i = 0; argv[i] != NULL; i++)
+				{
+					if (argv[i])
+					{
+						free(argv[i]);
+					}
+					continue;
+				}
+
+				free(argv);
+			}
+		}
+	}
+
+	free(command);
+	return (0);
+}
