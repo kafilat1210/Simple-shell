a5fca3451c8cf15d01d00d86e4a083a9e912f5e9
kafilat1210 <cafylhearty@gmail.com>
2023-12-16 11:39:24 -0800

    error gone

--- arguments_setter.c
+++ arguments_setter.c
@@ -12,36 +12,36 @@
  */
 char **set_args(char *command)
 {
-        string command_copy = NULL, token = NULL;
-        const char *delim = " \n";
-        int i, num_token = 0;
-        char **argv = NULL;
+	string command_copy = NULL, token = NULL;
+	const char *delim = " \n";
+	int i, num_token = 0;
+	char **argv = NULL;
 
-        command_copy = strdup(command);
-        token = strtok(command, delim);
-        if (_str_cmp(token, "env") == 0)
-        {
-                print_environment();
-                free(command_copy);
-                free(command);
-                return (NULL);
-        }
-        while (token != NULL)
-        {
-                num_token++;
-                token = strtok(NULL, delim);
-        }
-        num_token++;
-        argv =  malloc(sizeof(char *) * num_token);
-        token = strtok(command_copy, delim);
-        for (i = 0; token != NULL; i++)
-        {
-                argv[i] = malloc(_str_len(token) + 1);
-                strcpy(argv[i], token);
-                token = strtok(NULL, delim);
-        }
-        argv[i] = NULL;
-        free(command_copy);
-        free(command);
-        return (argv);
+	command_copy = strdup(command);
+	token = strtok(command, delim);
+	if (_str_cmp(token, "env") == 0)
+	{
+		print_environment();
+		free(command_copy);
+		free(command);
+		return (NULL);
+	}
+	while (token != NULL)
+	{
+		num_token++;
+		token = strtok(NULL, delim);
+	}
+	num_token++;
+	argv =  malloc(sizeof(char *) * num_token);
+	token = strtok(command_copy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		argv[i] = malloc(_str_len(token) + 1);
+		strcpy(argv[i], token);
+		token = strtok(NULL, delim);
+	}
+	argv[i] = NULL;
+	free(command_copy);
+	free(command);
+	return (argv);
 }
